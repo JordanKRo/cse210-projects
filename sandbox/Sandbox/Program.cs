5341bf3d70a484ec80e2fd35ff653ba40d431c4b
whataboutme42 <jordankrobison05@gmail.com>
2025-01-09 15:24:10 -0700

    playground

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Thingy thing = new Thingy();
+        thing.func();
+        
+    }
+}
+
+public class Thingy{
+    public Thingy(){
+        // create a thingy
+    }
+
+    public void func(){
+
     }
 }
