33988909507e4678c9e5e7afc013425055310b8d
whataboutme42 <jordankrobison05@gmail.com>
2025-01-13 21:08:05 -0700

    bond james is the name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,7 +9,7 @@ class Program
 
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
-
-        Console.WriteLine($"Your name is, {firstName} {lastName}.");
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
