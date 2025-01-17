89b8054a95ed1de331ed55a0c4f99ee3e5c6aec9
whataboutme42 <jordankrobison05@gmail.com>
2025-01-16 23:29:19 -0700

    functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using ToolBox;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        
+    }
+
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program");
+    }
+
+    static string PromptUsername(){
+        Console.Write("What is your username: ");
+        string username = Console.ReadLine() ?? "";
+        return username;
+    }
+
+    static int promptUserNumber(){
+        // See class for my implementation
+        int favNumber = InputValidation.PromptInt("What is your favorite number: ");
+        return favNumber;
+    }
+
+    static float SquareNumber(float input){
+        return (float) Math.Pow(input,2);
+    }
+
+    static void DisplayResult(string name, int number){
+        Console.WriteLine($"{name} the square of your number is {SquareNumber(number)}");
     }
 }
