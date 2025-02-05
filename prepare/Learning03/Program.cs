556260567726db427f9bf315846b653806956b5a
whataboutme42 <jordankrobison05@gmail.com>
2025-02-05 16:42:19 -0700

    done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(5);
+        Fraction third = new Fraction(3,4);
+        Fraction fourth = new Fraction(1,3);
+
+        Console.WriteLine(first);
+        Console.WriteLine(first.GetDecimal());
+
+        Console.WriteLine(second);
+        Console.WriteLine(second.GetDecimal());
+
+        Console.WriteLine(third);
+        Console.WriteLine(third.GetDecimal());
+
+        Console.WriteLine(fourth);
+        Console.WriteLine(fourth.GetDecimal());
     }
 }
