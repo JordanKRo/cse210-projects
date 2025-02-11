79350697be96c6e3f4eba178a5cdf1dad0938884
whataboutme42 <jordankrobison05@gmail.com>
2025-02-11 14:39:32 -0700

    verse appears to be done

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+
+        string testText = "For behold, it came to pass that the Lord spake unto my father, yea, even in a dream, and said unto him: Blessed art thou Lehi, because of the things which thou hast done; and because thou hast been faithful and declared unto this people the things which I commanded thee, behold, they seek to take away thy life.";
+        Verse verse = new Verse(testText, 2);
+        while(verse.GetNumberVisible() > 0){
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(verse.GetDisplay());
+
+            Console.ReadLine();
+            verse.HideWords(2);
+        }
+        
     }
 }
--- prove/Develop03/Verse.cs
+++ prove/Develop03/Verse.cs
@@ -1,6 +1,8 @@
 public class Verse{
-    int _verseNumber;
+    int _verseNumber = -1;
     List<Word> _words = new List<Word>();
+
+    Random randomSeed = new Random();
 
     public Verse(){
         
@@ -11,6 +13,13 @@ public class Verse{
         _verseNumber = verseNumber;
     }
 
+    public Verse(string originalText){
+        string[] tokens = originalText.Split(' ');
+        foreach(string token in tokens){
+            _words.Add(new Word(token));
+        }
+    }
+
     public Verse(string originalText, int verseNumber){
         _verseNumber = verseNumber;
 
@@ -19,13 +28,51 @@ public class Verse{
             _words.Add(new Word(token));
         }
     }
-    
+
+    public void HideWords(int count){
+        for(int i = 0;i < count;i++){
+            List<Word> visibleWords = GetVisibleWords();
+
+            if(visibleWords.Count == 0){
+                return;
+            }
+
+            int chosen = randomSeed.Next(visibleWords.Count);
+
+            visibleWords[chosen].SetVisibility(false);
+        }
+
+
+    }
 
     public string GetDisplay(){
-        string ret = $"{_verseNumber} ";
+        string ret = $"{_verseNumber} : ";
+
         foreach(Word word in _words){
-            ret += word;
+            ret += word + " ";
         }
+        return ret;
+    }
+
+    public int GetNumberVisible(){
+        int ret = 0;
+        foreach(Word word in _words){
+            if(word.IsVisible()){
+                ret++;
+            }
+        }
+
+        return ret;
+    }
+
+    private List<Word> GetVisibleWords(){
+        List<Word> ret = new List<Word>();
+        foreach(Word word in _words){
+            if(word.IsVisible()){
+                ret.Add(word);
+            }
+        }
+
         return ret;
     }
 }
