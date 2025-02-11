51b42cb3cfacac234cf1d7dc077e46c59579a8c1
whataboutme42 <jordankrobison05@gmail.com>
2025-02-11 15:36:29 -0700

    Scripture main functionality

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -2,8 +2,62 @@ public class Scripture{
     private Reference _reference;
     private List<Verse> _verses = new List<Verse>();
 
+    private Random _randomSeed = new Random();
+
     public Scripture(List<string> originalTexts, Reference reference){
+        _reference = reference;
+
+        for(int i = 0;i < originalTexts.Count;i++){
+            int verseNumber = i + reference.GetStart();
+
+            _verses.Add(new Verse(originalTexts[i], verseNumber));
+        }
         
+    }
+
+    public string GetDisplay(){
+        string ret = "";
+        foreach(Verse verse in _verses){
+            ret += verse.GetDisplay() + "\n\n";
+        }
+
+        return ret;
+    }
+
+
+    public void HideWords(int count){
         
+        while(count > 0){
+            List<Verse> visibleVerses = GetVersesStillVisible();
+
+            if (visibleVerses.Count == 0){
+                return;
+            }
+
+            int next = _randomSeed.Next(visibleVerses.Count);
+
+            visibleVerses[next].HideWords(1);
+        }
+    }
+
+    public int GetVisibleWordCount(){
+        int ret = 0;
+        
+        foreach(Verse verse in _verses){
+            ret += verse.GetNumberVisible();
+        }
+
+        return ret;
+    }
+
+    private List<Verse> GetVersesStillVisible(){
+        List<Verse> ret = new List<Verse>();
+
+        foreach(Verse verse in _verses){
+            if(verse.GetNumberVisible() > 0){
+                ret.Add(verse);
+            }
+        }
+        return ret;
     }
 }
