5667b2663d7f693d6a6707d451051457b3d02d59
whataboutme42 <jordankrobison05@gmail.com>
2025-02-11 14:56:48 -0700

    Started Reference and scripture

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,10 +1,45 @@
 public class Reference{
-    private string _book;
-    private int _chapter;
-    private int _start;
-    private int _end;
+    private string _book = "";
+    private int _chapter = -1;
+    private int _start = -1;
+    private int _end = -1;
 
     public Reference(string book, int chapter, int start, int end){
+        _book = book;
+        _chapter = chapter;
+        _start = start;
+        _end = end;
+    }
 
+    public Reference(string book, int chapter, int start){
+        _book = book;
+        _chapter = chapter;
+        _start = start;
+    }
+
+    public override string ToString()
+    {
+        if(_end == -1){
+            return $"{_book} {_chapter} : {_start}]";
+        }else{
+            return $"{_book} {_chapter} : {_start}-{_end}]";
+        }
+        
+    }
+
+    public int GetStart(){
+        return _start;
+    }
+
+    public int GetEnd(){
+        return _end;
+    }
+
+    public int GetChapter(){
+        return _chapter;
+    }
+
+    public string GetBook(){
+        return _book;
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,8 +1,9 @@
 public class Scripture{
     private Reference _reference;
-    private List<Verse> _verses;
+    private List<Verse> _verses = new List<Verse>();
 
-    public Scripture(Reference reference, List<Verse> verses){
-
+    public Scripture(List<string> originalTexts, Reference reference){
+        
+        
     }
 }
