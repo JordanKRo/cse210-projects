b07a809d09a0655a2ae01ed3ba7551f3af96b241
whataboutme42 <jordankrobison05@gmail.com>
2025-02-11 14:16:02 -0700

    word class finished

--- prove/Develop03/Verse.cs
+++ prove/Develop03/Verse.cs
@@ -1,11 +1,29 @@
 public class Verse{
-    int verseNumber;
-    string originalText;
-    List<Word> words;
+    int _verseNumber;
+    List<Word> _words = new List<Word>();
+
+    public Verse(){
+        
+    }
+
+    public Verse(List<Word> words, int verseNumber){
+        _words = words;
+        _verseNumber = verseNumber;
+    }
+
+    public Verse(string originalText, int verseNumber){
+        _verseNumber = verseNumber;
+
+        string[] tokens = originalText.Split(' ');
+        foreach(string token in tokens){
+            _words.Add(new Word(token));
+        }
+    }
+    
 
     public string GetDisplay(){
-        string ret = $"{verseNumber} ";
-        foreach(Word word in words){
+        string ret = $"{_verseNumber} ";
+        foreach(Word word in _words){
             ret += word;
         }
         return ret;
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -2,6 +2,20 @@ public class Word{
     string _theWord;
     bool _visible = true;
 
+    public Word(){
+        _theWord = "";
+        _visible = true;
+    }
+    public Word(string text){
+        _theWord = text;
+        _visible = true;
+    }
+
+    public Word(string text, bool visible){
+        _theWord = text;
+        _visible = visible;
+    }
+
     public override string ToString()
     {
         if (_visible){
