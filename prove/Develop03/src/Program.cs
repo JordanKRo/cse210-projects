9a85d700ea68c9a6d1249ba9df4062d62ba7f0a7
whataboutme42 <jordankrobison05@gmail.com>
2025-02-14 00:09:32 -0700

    src folder

--- prove/Develop03/src/Program.cs
+++ prove/Develop03/src/Program.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         // OS compliant path
         string basePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
@@ -17,7 +17,7 @@ class Program
         
         // check if api is enabled, then check if can obtain book cache.
         
-        Scripture scripture = library.GetScripture();
+        Scripture scripture = await library.GetScripture();
         
         Comprehension comprehension = new Comprehension(scripture);
 
