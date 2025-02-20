262483028080244e255998bdcd9324050c6cb0d4
whataboutme42 <jordankrobison05@gmail.com>
2025-02-19 23:36:36 -0700

    more

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,8 +6,10 @@ class Program
     {
         Assignment math = new MathAssignment("Jordan", "33333", "4.2", "1,5,7,9,11,14-20");
         Assignment writing = new WritingAssignment("Jordan", "rhetorical", "Citing Sources");
+        Assignment cse = new Assignment("Jordan", "Inheritance");
 
         Console.WriteLine(math.GetSummary());
         Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(cse.GetSummary());
     }
 }
